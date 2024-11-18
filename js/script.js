4a11c9fe728e6165d5b3d8441e5fefa2a4d03efb
Andy Simota <andyiulian.simota@gmail.com>
2024-11-18 17:41:05 +0100

    add names list and update email display with icons

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-// https://flynn.boolean.careers/exercises/api/random/mail
+import { names } from "./db.js";
 
 const email = document.getElementById("email");
 const btnGenera = document.getElementById("btnGenera");
 const btnReset = document.querySelector(".btn-dark");
+const namesList = document.getElementById("namesList");
 let newArray = [];
 
 function getEmailX10() {
   for (let i = 0; i < 10; i++) {
-    getEmail();
+    getEmail(i);
   }
 }
 
-function getEmail() {
+function getEmail(index) {
   axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((risposta) => {
-      email.innerHTML += `<li>La tue email e':  ${risposta.data.response}</li>`;
+      email.innerHTML += `<li><i class="fa-solid fa-envelope"></i>  ${risposta.data.response}</li>`;
       console.log(risposta.data.response);
+      namesList.innerHTML += `<li><i class="fa-solid fa-user"></i>  ${names[index]}</li>`;
     })
     .catch((error) => {
       console.log(error);
@@ -28,6 +30,7 @@ function getEmail() {
 
 function reset() {
   email.innerHTML = "";
+  namesList.innerHTML = "";
 }
 
 btnGenera.addEventListener("click", getEmailX10);
